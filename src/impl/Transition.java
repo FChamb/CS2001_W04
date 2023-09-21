4b3c456b54b4996e2631917b7d7470b488fe0888
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-09-21 10:25:58 +0100

    First commit added all code

---
+++ src/impl/Transition.java
@@ -0,0 +1,35 @@
+package impl;
+
+import interfaces.ITransition;
+
+/**
+ * Class representing a single transition for an FSM, equivalent to a row in a transition table.
+ *
+ */
+public class Transition implements ITransition {
+
+    @Override
+    public int getCurrentState() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public char getInput() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public char getOutput() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNextState() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
